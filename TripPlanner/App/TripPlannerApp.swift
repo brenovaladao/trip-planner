0e118827bc63ca56bec8ab2359b46d3bed2f7a47
Breno Valadão <brenovaladao@Brenos-Air.Home>
2023-12-06 10:05:06 +0000

    chore: project creation and initial setup

---
+++ TripPlanner/App/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Breno Valadão on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
