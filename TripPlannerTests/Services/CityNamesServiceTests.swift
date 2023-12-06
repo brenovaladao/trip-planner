68d6a41405d2d3cd66b4b9c6db99683f66db18f2
Breno Valadão <brenovvaladao@gmail.com>
2023-12-06 15:25:12 +0000

    test: add test for init and empty response

--- TripPlannerTests/Services/CityNamesServiceTests.swift
+++ TripPlannerTests/Services/CityNamesServiceTests.swift
@@ -10,21 +10,33 @@ import XCTest
 
 @MainActor
 final class CityNamesServiceTests: XCTestCase {
-
+    func test_fetchCityNames_noSideEffectsOnInitialization() {
+        let (_, spy) = makeSUT(mockResult: .success([]))
+        XCTAssertTrue(spy.messages.isEmpty)
+    }
+    
+    func test_fetchCityNames_emptyOnEmptyMock() async throws {
+        let (sut, spy) = makeSUT()
+        
+        let names = try await sut.fetchCityNames(searchType: .departure)
+        
+        XCTAssertEqual(spy.messages, [.fetchConnections])
+        XCTAssertTrue(names.isEmpty)
+    }
 }
 
 private extension CityNamesServiceTests {
     func makeSUT(
-        mockResult: Result<[FlightConnection], Error>,
+        mockResult: Result<[FlightConnection], Error> = .success([]),
         file: StaticString = #filePath,
         line: UInt = #line
-    ) -> CityNamesService {
+    ) -> (CityNamesService, FlightConnectionsServiceSpy) {
         let spy = FlightConnectionsServiceSpy(mockResult)
         let sut = CityNamesService(flightsLoader: spy)
         
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(spy, file: file, line: line)
 
-        return sut
+        return (sut, spy)
     }
 }
